ca84f0e433ec4ccc3a74471b494c304763600b2b
Ryu-0402 <yongjingliu7@gmail.com>
2026-02-13 12:04:14 +0900

    before login function

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import "../global.css";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+
+export default function RootLayout() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  );
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require("nativewind/preset")],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
+
